5e679470f69a031f2c92b532fa0890c9ff1b43e8
ej-Zi <eva@fichtensepp.de>
2026-09-17 18:19:52 +0200

    fix nvim

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -20,17 +20,6 @@ vim.api.nvim_create_autocmd("TermClose", {
 
 require("lazy").setup("plugins")
 
-require'nvim-treesitter.configs'.setup {
-    sync_install = false,
-    auto_install = true,
-
-    highlight = {
-        enable = true,
-        disable = {"latex"},
-        additional_vim_regex_highlighting = {"latex"},
-    }
-}
-
 require('nvim-ts-autotag').setup({
   opts = {
     -- Defaults
